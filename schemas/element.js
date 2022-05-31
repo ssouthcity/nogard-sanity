8a8c5368894d493e2c8e23b022c3472bfcb8b64b
Stian Sørby <ssouthcity@gmail.com>
2022-06-01 01:47:17 +0200

    added element resource

---
+++ schemas/element.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'element',
+    type: 'document',
+    title: 'Element',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name',
+        },
+        {
+            name: 'icon',
+            type: 'image',
+            title: 'Icon',
+        },
+    ],
+}
