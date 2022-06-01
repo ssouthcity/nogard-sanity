270bce998dec044bd73bb9e9036caa9973e9d23f
Stian Sørby <ssouthcity@gmail.com>
2022-06-01 02:18:56 +0200

    added dragon resource

---
+++ schemas/dragon.js
@@ -0,0 +1,30 @@
+export default {
+    name: 'dragon',
+    type: 'document',
+    title: 'Dragon',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name',
+        },
+        {
+            name: 'elements',
+            type: 'array',
+            of: [{ type: 'relation', to: [{ type: 'element' }] }],
+            title: 'Elements',
+        },
+        {
+            name: 'breeding_elements',
+            type: 'array',
+            of: [{ type: 'relation', to: [{ type: 'element' }] }],
+            title: 'Breeding Elements',
+        },
+        {
+            name: 'breeding_requirement',
+            type: 'array',
+            of: [{ type: 'relation', to: [{ type: 'element' }, { type: 'dragon' }] }],
+            title: 'Breeding Requirement',
+        },
+    ],
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,11 +1,13 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+import dragon from './dragon'
 import element from './element'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
+    dragon,
     element,
   ]),
 })
